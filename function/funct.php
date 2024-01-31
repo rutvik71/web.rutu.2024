0530467ac1fdc927441a6c964f150602be577bf8
nasiimu <nakamattenasiimu0@gmail.com>
2024-01-31 17:47:17 +0300

    php

---
+++ function/funct.php
@@ -0,0 +1,9 @@
+<?php
+function hello_world(){
+    $firstname = 'bob';
+    $salary = 398.5;
+    $is_married = true;
+ echo "first name : $firstname salary: $salary  are you married: $is_married ";
+}
+hello_world();
+?>
